9675ab4fa0ee83d5a6316f5db1284df7d6215d06
TahaMagdy <t.tahamagdyt@outlook.com>
2021-01-24 16:02:04 +0200

    [Bug] aya starts at 1 instead of 0

--- sura_to_latex.py
+++ sura_to_latex.py
@@ -8,6 +8,7 @@ sura = q.quran.get_sura(q.quran.get_sura_number('النساء'), with_tashkeel=T
 f = open('out.tex', 'w')
 out = []
 for number, text in enumerate(sura):
+    number += 1
     latex_number = f'{{\\tiny\colorbox{{cl_aya}}{{{number}}}}}'
     line = latex_number + ' ' + text + '\n'
     f.write(line)
