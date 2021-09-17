bb451c2ecafa15a8744caf20dc79677e7ebc5d4a
TahaMagdy <t.tahamagdyt@outlook.com>
2021-09-18 00:42:41 +0200

    preparing RevisionWird

--- Memorise/RevisionWird.py
+++ Memorise/RevisionWird.py
@@ -7,3 +7,12 @@ class RevisionWird:
     def __init__(self, parts_count=6) -> None:
         self.parts_count          = parts_count
         self.memorized_suras_list = open('Hifz_taha.txt').readlines()
+
+    def print_wird(self):
+        print('wird')
+
+
+
+if __name__ == '__main__':
+    wird = RevisionWird()
+    wird.print_wird()
