afb784d6fad66199dbdf35dfb9c95bb1c7a0b9a6
TahaMagdy <t.tahamagdyt@outlook.com>
2021-09-18 00:45:06 +0200

    add compute_wird()

--- Memorise/RevisionWird.py
+++ Memorise/RevisionWird.py
@@ -7,12 +7,13 @@ class RevisionWird:
     def __init__(self, parts_count=6) -> None:
         self.parts_count          = parts_count
         self.memorized_suras_list = open('Hifz_taha.txt').readlines()
+        self.wird = dict()  # for example {'1:6' : '9999 البقرة'}
 
-    def print_wird(self):
+    def compute_wird(self):
         print('wird')
 
 
 
 if __name__ == '__main__':
     wird = RevisionWird()
-    wird.print_wird()
+    wird.compute_wird()
