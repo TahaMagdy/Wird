1ed0d52765d24c7285fddb75eb981dcbfd59d275
TahaMagdy <t.tahamagdyt@outlook.com>
2021-09-18 00:48:44 +0200

    sync

--- Memorise/RevisionWird.py
+++ Memorise/RevisionWird.py
@@ -4,10 +4,13 @@ import pyquran as q
 
 
 class RevisionWird:
-    def __init__(self, parts_count=6) -> None:
+    def __init__(self
+            , parts_count=6
+            , memorized_suras_names_file='Hifz_taha.txt'):
+        self.wird                 = dict()  # for example {'1:6' : '9999 البقرة'}
         self.parts_count          = parts_count
-        self.memorized_suras_list = open('Hifz_taha.txt').readlines()
-        self.wird = dict()  # for example {'1:6' : '9999 البقرة'}
+        self.memorized_suras_list = open(memorized_suras_names_file).readlines()
+
 
     def compute_wird(self):
         print('wird')
