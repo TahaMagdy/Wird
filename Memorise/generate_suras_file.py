2f0d32875c734a6101a0ab67bfee0d9063bcc7c8
TahaMagdy <t.tahamagdyt@outlook.com>
2021-09-18 20:31:49 +0200

    change output file name

--- Memorise/generate_suras_file.py
+++ Memorise/generate_suras_file.py
@@ -3,7 +3,7 @@ sys.path.append('..')
 
 import pyquran as q
 
-f = open('list.txt', 'w')
+f = open('suras_names_all_quran.txt', 'w')
 for i in range(1, 114+1):
     name = q.quran.get_sura_name(i)
     f.write(name + '\n')
