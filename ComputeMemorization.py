41da0d6911964d53f0297a96aa5ad4b9739ea8e4
Taha Magdy <tahamagdy@Tahas-MacBook-Pro.local>
2023-06-28 01:20:01 +0300

    add ComputeMemorization.py

---
+++ ComputeMemorization.py
@@ -0,0 +1,20 @@
+import MemorizedQuran
+
+
+get_memorized_or_not_memorized = \
+    lambda predicate, your_quran: [ (name, number_of_ayat) for name, number_of_ayat in your_quran.items()
+                                    if predicate(number_of_ayat) ]
+
+
+def get_memorized(how_much_quran_do_you_know):
+    memorized     = get_memorized_or_not_memorized(lambda n: n > 0, how_much_quran_do_you_know)
+    return memorized
+
+
+# TESTING
+
+memorized     = get_memorized_or_not_memorized(lambda n: n > 0, MemorizedQuran.quran_taha)
+not_memorized = get_memorized_or_not_memorized(lambda n: n == 0, MemorizedQuran.quran_taha)
+
+assert len(memorized) + len(not_memorized) == 114 \
+            , 'memorized + not_memorized != 114'
